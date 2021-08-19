1b629e1106cbeec8cf8c7426b732ce901f350185
Juan Acero <dev.acerojuan@gmail.com>
2021-08-19 12:32:13 -0500

    useEffect with custom hook

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import FormWithCustomHook from "./Components/02 - useEffect/FormWithCustomHook";
 // import CounterApp from "./Components/01 - useState/CounterApp";
 // import CounterWithCustomHook from "./Components/01 - useState/CounterWithCustomHook";
-import SimpleForm from "./Components/02 - useEffect/SimpleForm";
+// import SimpleForm from "./Components/02 - useEffect/SimpleForm";
 // import HooksApp from "./HooksApp"
 
-ReactDOM.render(<SimpleForm />, document.getElementById("root"));
+ReactDOM.render(<FormWithCustomHook />, document.getElementById("root"));
